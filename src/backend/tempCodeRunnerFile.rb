8bcceb394c89daf327154c7df4be8403616bd611
Telmo Ribeiro <61145755+TelmoRibeiro@users.noreply.github.com>
2025-07-07 01:36:25 +0100

    POST is running
    running without database

--- src/backend/tempCodeRunnerFile.rb
+++ src/backend/tempCodeRunnerFile.rb
@@ -34,10 +34,9 @@ post '/add_run' do
   distance, start_time, end_time, date, location = get_parameters()
   error_on_missing_parameters(distance, start_time, end_time, date, location)
 
-  # puts "d: #{distance}\nst: #{start_time}\net: #{end_time}\ndt: #{date}\nl: #{location}" # @ telmo - just for testing
+  puts "d: #{distance}\nst: #{start_time}\net: #{end_time}\ndt: #{date}\nl: #{location}" # @ telmo - just for testing
 
   content_type :json
-  status 200
   { message: 'Run Added Sucessfully' }.to_json
 end
 
@@ -46,16 +45,14 @@ def get_parameters()
   begin
     payload = JSON.parse(request.body.read)
   rescue JSON::ParserError => error
-    halt 400,
-      { 'Content-Type' => 'application/json'},
-      { error: "Invalid JSON: #{error.message}" }.to_json
+    halt 400, { 'Content-Type' => 'application/json'}, { error: "Invalid JSON: #{error.message}" }.to_json
   end
   distance   = payload['distance']
   start_time = payload['start_time']
   end_time   = payload['end_time']
   date       = payload['date']
   location   = payload['location']
-  [distance, start_time, end_time, date, location]
+  return [distance, start_time, end_time, date, location]
 end
 
 def error_on_missing_parameters(distance, start_time, end_time, date, location)
@@ -65,9 +62,5 @@ def error_on_missing_parameters(distance, start_time, end_time, date, location)
   missing_parameters << 'end_time'   if end_time.nil?
   missing_parameters << 'date'       if date.nil?
   missing_parameters << 'location'   if location.nil?
-  unless missing_parameters.empty?
-    halt 400,
-      { 'Content-Type' => 'application/json' },
-      { error: "Missing Parameters: #{missing_parameters.join(', ')}" }.to_json
-  end
+  halt 400, { 'Content-Type' => 'application/json' }, { error: "Missing Parameters: #{missing_parameters.join(', ')}" }
 end
